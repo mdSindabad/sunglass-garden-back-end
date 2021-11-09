dd271b16dbae52ed5f3c9510e538b66af693f8e5
Md. Sindabad <pavel.sindabad@gmail.com>
2021-11-10 00:01:25 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 // initialize express app
 const app = express();
@@ -9,9 +11,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("From Sunglass Garden Server...")
-})
+// database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.gofan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('sunglass_garden');
+        const products = database.collection('products');
+
+
+        // get all services
+        app.get('/products', async (req, res) => {
+            const cursor = await products.find({});
+            const data = await cursor.toArray();
+            res.send(data)
+        });
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 const port = process.env.PORT || 5000;
